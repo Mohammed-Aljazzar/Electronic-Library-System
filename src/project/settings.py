18807cf29da7927139b132340c2e7a1694a62193
Mohammed-Aljazzar <m.i.aljazzar19@gmail.com>
2025-03-25 11:39:22 +0200

    Prepare project for Render deployment 11

--- src/project/settings.py
+++ src/project/settings.py
@@ -86,13 +86,27 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}',  
-        conn_max_age=600
-    )
-}
-
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}',  
+#         conn_max_age=600
+#     )
+# }
+DATABASE_URL = os.getenv('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
